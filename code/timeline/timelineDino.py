c5f4e32116033d82ac3a0dbba3733e437b027530
Obermühlner Adrian <aobermuhlner@gmail.com>
2023-11-09 11:20:48 +0100

    dino templates

---
+++ code/timeline/timelineDino.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw
+
+# Open the timeline image
+image = Image.open("timeline3.png")
+
+# Get the width and height of the image
+width, height = image.size
+
+# Define the bar parameters
+bar_height = 5
+bar_color = (260, 260, 260)  # (255, 0, 0)
+
+###############################################################################
+
+# Define the start and end years
+start_year = 155
+dino_name = "diplodocus"
+
+###############################################################################
+start_x = int(17 - 55 + 8.7 * (252 - start_year))
+
+# Load the image to paste
+# Replace with your image file
+image_to_paste = Image.open("timeline_rex.jpeg")
+
+# Resize the image while maintaining aspect ratio
+aspect_ratio = image_to_paste.width / image_to_paste.height
+new_width = int(28 * aspect_ratio)
+resized_image = image_to_paste.resize((new_width, 28))
+
+# Calculate the position to paste
+# Assuming you want to center the image between start_x and end_x
+paste_x = start_x
+paste_y = image.height - 80
+
+# Paste the image onto the timeline image
+if resized_image.mode in ('RGBA', 'LA') or (resized_image.mode == 'P' and 'transparency' in resized_image.info):
+    # Use alpha channel as a mask
+    mask = resized_image.split()[3]
+    image.paste(resized_image, (paste_x, paste_y), mask)
+else:
+    # Paste without a mask
+    image.paste(resized_image, (paste_x, paste_y))
+
+# Save the final image
+image.save(dino_name + "_timline.png")
--- code/timeline/timeline_creator.py
+++ code/timeline/timeline_creator.py
@@ -1,23 +1,58 @@
 from PIL import Image, ImageDraw
 
+# Open the timeline image
 image = Image.open("timeline3.png")
 
+# Get the width and height of the image
 width, height = image.size
 
+# Define the bar parameters
 bar_height = 5
-bar_color = (255, 0, 0)
+bar_color = (260, 260, 260)  # (255, 0, 0)
 
-start_year = 157
-end_year = 147.7
+###############################################################################
 
+# Define the start and end years
+start_year = 155
+end_year = 145
+dino_name = "diplodocus"
 
+###############################################################################
+
+y_height = 68
+# Calculate the X coordinates for the bar
 start_x = int(17 + 8.7 * (252 - start_year))
 end_x = int(1638 + 8.7 * (66 - end_year))
 
+# Calculate the width of the bar
 bar_width = end_x - start_x
+
+# Create the bar image
 bar_image = Image.new('RGB', (bar_width, bar_height), color=bar_color)
 
+# Paste the bar onto the timeline image
+image.paste(bar_image, (start_x, height - y_height))
+# image.paste(bar_image, (start_x, height - 52))
 
-image.paste(bar_image, (start_x, height - 2*bar_height))
+# Create a draw object to draw vertical lines
+draw = ImageDraw.Draw(image)
 
-image.save("timeline_with_bar.png")
+# Define the length of the vertical lines
+vertical_line_length = 28
+
+# Draw a vertical line at the start
+start_line_x = start_x
+start_line_y1 = height - 80
+start_line_y2 = start_line_y1 + vertical_line_length
+draw.line([(start_line_x, start_line_y1),
+          (start_line_x, start_line_y2)], fill=bar_color, width=5)
+
+# Draw a vertical line at the end
+end_line_x = start_x + bar_width
+end_line_y1 = height - 80
+end_line_y2 = end_line_y1 + vertical_line_length
+draw.line([(end_line_x, end_line_y1), (end_line_x, end_line_y2)],
+          fill=bar_color, width=5)
+
+# Save the final image
+image.save(dino_name + "_timline.png")
